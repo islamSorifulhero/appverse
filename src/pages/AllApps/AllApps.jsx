b9a24d0d44285c3aa47b035463a702132ca89b32
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-10-09 07:53:33 +0600

    Search and total Apps found

--- src/pages/AllApps/AllApps.jsx
+++ src/pages/AllApps/AllApps.jsx
@@ -13,10 +13,30 @@ const AllApps = () => {
 
   return (
     <div className="my-12 px-4 md:px-8">
-      <h2 className="text-3xl font-semibold text-center mb-8 text-gray-800">
-        All Apps
-      </h2>
-
+      <h2 className="text-3xl font-semibold text-center text-gray-800">Our All Applications</h2>
+      <p className="text-center mb-8 text-[#627382]">Explore All Apps on the Market developed by us. We code for Millions</p>
+      <div className="flex justify-between items-center mb-4">
+        <div>
+          <h2>(20) Apps Found</h2>
+        </div>
+        <div>
+          <label className="input">
+            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="Search Apps" />
+          </label>
+        </div>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
         {apps.map((app) => (
           <div
